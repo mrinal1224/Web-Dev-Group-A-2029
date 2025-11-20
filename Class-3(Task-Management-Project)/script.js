2c9323365c3aee97d060a2facee448a3a56a4cb5
mrinal1224 <iammrinalkc@gmail.com>
2025-11-20 15:19:22 +0530

    tickets with task

--- Class-3(Task-Management-Project)/script.js
+++ Class-3(Task-Management-Project)/script.js
@@ -1,11 +1,11 @@
 // flags
 let modalFlag = false;
 
-
 // Elements
 const addBtn = document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
-
+const modalTaskArea = document.querySelector('.textArea-cont')
+const mainCont = document.querySelector('.main-cont')
 
 
 // this section opens and closes the modal
@@ -18,3 +18,38 @@ addBtn.addEventListener("click", function () {
     modalFlag = false;
   }
 });
+
+
+// Add the Task and createTicket
+modalCont.addEventListener('keydown' , function(e){
+   
+   if(e.key=='Shift'){
+    let task = modalTaskArea.value
+    // generate the ticket
+    createTicket(task)
+    modalCont.style.display = "none";
+    modalFlag = false;
+
+   }
+
+   // we will do nothing
+})
+
+// function to create or generate the ticket
+
+function createTicket(ticketTask){
+  const ticketCont =   document.createElement('div')
+  ticketCont.setAttribute('class' , 'ticket-cont')
+
+ ticketCont.innerHTML= ` <div class="ticket-color"></div>
+ <div class="ticket-id">1234567</div>
+ <div class="task-area">${ticketTask}</div>
+ <div class="ticket-lock">
+    <i class="fa-solid fa-lock"></i>
+ </div>`
+
+
+
+ mainCont.appendChild(ticketCont)
+
+}
