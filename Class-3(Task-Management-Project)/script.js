07d6fb1a86fa888ad0b1e82b3f65335dab40e6f8
mrinal1224 <iammrinalkc@gmail.com>
2025-11-25 15:36:47 +0530

    open and close the lock

--- Class-3(Task-Management-Project)/script.js
+++ Class-3(Task-Management-Project)/script.js
@@ -4,15 +4,15 @@ let modalFlag = false;
 // Elements
 const addBtn = document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
-const modalTaskArea = document.querySelector('.textArea-cont')
-const mainCont = document.querySelector('.main-cont')
-const allPriorityColors = document.querySelectorAll('.priority-color')
+const modalTaskArea = document.querySelector(".textArea-cont");
+const mainCont = document.querySelector(".main-cont");
+const allPriorityColors = document.querySelectorAll(".priority-color");
+
+let closeLock =  'fa-lock'
+let openLock = 'fa-lock-open'
 
 
-let ticketColor = 'lightpink'
-
-
-
+let ticketColor = "lightpink";
 
 // this section opens and closes the modal
 addBtn.addEventListener("click", function () {
@@ -25,54 +25,64 @@ addBtn.addEventListener("click", function () {
   }
 });
 
-
 // Add the Task and createTicket
-modalCont.addEventListener('keydown' , function(e){
-   
-   if(e.key=='Shift'){
-    let task = modalTaskArea.value
+modalCont.addEventListener("keydown", function (e) {
+  if (e.key == "Shift") {
+    let task = modalTaskArea.value;
     // generate the ticket
-    createTicket(task)
+    createTicket(task);
     modalCont.style.display = "none";
     modalFlag = false;
+  }
 
-   }
-
-   // we will do nothing
-})
+  // we will do nothing
+});
 
 // function to create or generate the ticket
 
-function createTicket(ticketTask){
-  const ticketCont =   document.createElement('div')
-  ticketCont.setAttribute('class' , 'ticket-cont')
+function createTicket(ticketTask) {
+  const ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
 
- ticketCont.innerHTML= ` <div style="background-color:${ticketColor};" class="ticket-color"></div>
+  ticketCont.innerHTML = ` <div style="background-color:${ticketColor};" class="ticket-color"></div>
  <div class="ticket-id">1234567</div>
  <div class="task-area">${ticketTask}</div>
  <div class="ticket-lock">
     <i class="fa-solid fa-lock"></i>
- </div>`
+ </div>`;
 
+  mainCont.appendChild(ticketCont);
+  // handleLock
 
-
- mainCont.appendChild(ticketCont)
-
+  handleLock(ticketCont);
 }
 
+allPriorityColors.forEach(function (colorElem) {
+  colorElem.addEventListener("click", function () {
+    allPriorityColors.forEach(function (priorityColor) {
+      priorityColor.classList.remove("active");
+    });
 
-allPriorityColors.forEach(function(colorElem){
-  colorElem.addEventListener('click' , function(){
-    
-     allPriorityColors.forEach(function(priorityColor){
-        priorityColor.classList.remove("active")
-     })
-    
-     colorElem.classList.add('active')
-     const color =  colorElem.classList[0]
-     ticketColor = color
-  })
-})
+    colorElem.classList.add("active");
+    const color = colorElem.classList[0];
+    ticketColor = color;
+  });
+});
 
+// handle the lock
 
+function handleLock(ticket) {
+  const ticketLockConatainer = ticket.querySelector(".ticket-lock");
+   
+ const ticketLock = ticketLockConatainer.children[0]
 
+ ticketLock.addEventListener('click' , function(){
+   if(ticketLock.classList.contains(closeLock)){
+       ticketLock.classList.remove(closeLock)
+       ticketLock.classList.add(openLock)
+   }else{
+    ticketLock.classList.remove(openLock)
+    ticketLock.classList.add(closeLock)
+   }
+ })
+}
