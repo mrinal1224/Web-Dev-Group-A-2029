2f079ada43f08efaa5e3f2b60b6356b978927f71
mrinal1224 <iammrinalkc@gmail.com>
2026-01-06 15:34:04 +0530

    async setTimeout

---
+++ Class-10 (Asynchornous Programing-1)/script.js
@@ -0,0 +1,29 @@
+let a = 5
+
+let b=7
+
+
+console.log(a) // 5
+
+
+function sayHello(){
+    for(let i=0 ; i<10 ; i++){
+        console.log('hello')
+    }
+   
+}
+// sayHello() // hello
+
+// setTimeout
+
+setTimeout(sayHello)
+
+
+
+
+console.log(b) // 7
+
+
+//5
+
+
