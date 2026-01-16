a273f7825c29f0cbf6eca464b40d957fa3cd9e2e
mrinal1224 <iammrinalkc@gmail.com>
2026-01-16 15:56:14 +0530

    promise chain

--- Class-13(Async Await)/promiseChain.js
+++ Class-13(Async Await)/promiseChain.js
@@ -1,23 +1,46 @@
 function placeOrder(drink) {
-    return new Promise(function (resolve, reject) {
-      if (drink === "coffee") {
-        resolve("Order taken for coffee");
-      } else {
-        reject("Cannot take the Order");
-      }
-    });
-  }
-  
-  function processOrder(orderPlaced) {
-    return new Promise(function (resolve) {
-      setTimeout(function () {
-        resolve(`${orderPlaced} and Served.`);
-      }, 10000);
-    });
-  }
-  
-  function generateBill() {
-    return new Promise(function (resolve) {
-      resolve(`Bill Generated`);
-    });
-  }
+  return new Promise(function (resolve, reject) {
+    if (drink === "coffee") {
+      resolve("Order taken for coffee");
+    } else {
+      reject("Cannot take the Order");
+    }
+  });
+}
+
+function processOrder(orderPlaced) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(`${orderPlaced} and Served.`);
+    }, 1000);
+  });
+}
+
+function generateBill() {
+  return new Promise(function (resolve) {
+    resolve(`Bill Generated`);
+  });
+}
+
+// Promise Chaining
+
+placeOrder("coffee")
+  .then(function (orderTaken) {
+    console.log(orderTaken);
+    return orderTaken;
+  })
+  .then(function (orderTakenData) {
+    const finalOrder = processOrder(orderTakenData);
+    return finalOrder;
+  })
+  .then(function (finalOrderData) {
+    console.log(finalOrderData);
+    const bill = generateBill();
+    return bill;
+  })
+  .then(function (billData) {
+    console.log(billData);
+  })
+  .catch(function (error) {
+      console.log(error)
+  });
