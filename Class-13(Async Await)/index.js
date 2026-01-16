735052a61b47ae7454109f93d5a25ebfef2eeaf7
mrinal1224 <iammrinalkc@gmail.com>
2026-01-16 15:16:17 +0530

    functions

---
+++ Class-13(Async Await)/index.js
@@ -0,0 +1,22 @@
+function placeOrder(drink, successCallback, errorCallback) {
+    setTimeout(() => {
+      if (drink === "coffee") {
+        successCallback("Order taken");
+      } else {
+        errorCallback("Cannot take the Order");
+      }
+    }, 1000);
+  } // 1
+  
+  function processOrder(orderPlaced, callback) {
+    setTimeout(() => {
+      callback(`${orderPlaced} and Served.`);
+    }, 1000);
+  } // 2
+  
+  function generateBill(callback) {
+    setTimeout(() => {
+      callback("Bill Generated");
+    }, 1000);
+  } // 3
+  
