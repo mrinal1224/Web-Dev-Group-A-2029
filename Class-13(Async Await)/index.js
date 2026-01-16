cb7ee1fdb43f5dbfc07c11a18938104a0610a3d0
mrinal1224 <iammrinalkc@gmail.com>
2026-01-16 15:36:39 +0530

    callback hell

--- Class-13(Async Await)/index.js
+++ Class-13(Async Await)/index.js
@@ -1,22 +1,35 @@
 function placeOrder(drink, successCallback, errorCallback) {
-    setTimeout(() => {
-      if (drink === "coffee") {
-        successCallback("Order taken");
-      } else {
-        errorCallback("Cannot take the Order");
-      }
-    }, 1000);
-  } // 1
-  
-  function processOrder(orderPlaced, callback) {
-    setTimeout(() => {
-      callback(`${orderPlaced} and Served.`);
-    }, 1000);
-  } // 2
-  
-  function generateBill(callback) {
-    setTimeout(() => {
-      callback("Bill Generated");
-    }, 1000);
-  } // 3
-  
+  setTimeout(() => {
+    if (drink === "coffee") {
+      successCallback("Order taken");
+    } else {
+      errorCallback("Cannot take the Order");
+    }
+  }, 1000);
+} // 1
+
+function processOrder(orderPlaced, callback) {
+  setTimeout(() => {
+    callback(`${orderPlaced} and Served.`);
+  }, 1000);
+} // 2
+
+function generateBill(callback) {
+  setTimeout(() => {
+    callback("Bill Generated");
+  }, 1000);
+} // 3
+
+// Execute these functions one by one in order
+
+function serveOrder() {
+  placeOrder("coffee", function (orderPlaced) {
+    processOrder(orderPlaced, function (finalOrder) {
+      generateBill(function (billData) {
+        // Now you wil get stuck in callback Hell
+      });
+    });
+  });
+}
+
+serveOrder();
