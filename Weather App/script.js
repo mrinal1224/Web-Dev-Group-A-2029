9ff3bc13431fbfb81df03c05f63029dac4a0021d
mrinal1224 <iammrinalkc@gmail.com>
2026-01-22 16:25:47 +0530

    PUSH

--- Weather App/script.js
+++ Weather App/script.js
@@ -3,7 +3,7 @@
 const searchForm = document.querySelector(".search-form");
 const inputField = document.querySelector(".searchField");
 const tempFiled = document.querySelector(".temp");
-const locationField = document.querySelector('.location')
+const locationField = document.querySelector(".location");
 
 searchForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -14,14 +14,19 @@ searchForm.addEventListener("submit", function (e) {
 let target = "Mumbai";
 
 async function fetchData(location) {
-  let res = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=35af7ff606db422880d141328231305&q=${location}&aqi=no`
-  );
+  try {
+    let res = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=35af7ff606db422880d141328231305&q=${location}&aqi=no`
+    );
 
-  const data = await res.json(); // converts readableJsonStream to Js Objects
+    const data = await res.json(); // converts readableJsonStream to Js Objects
 
-  console.log(data);
+    console.log(data);
 
-  tempFiled.innerText = data.current.temp_c;
-  locationField.innerText = data.location.name
+    tempFiled.innerText = data.current.temp_c;
+    locationField.innerText = data.location.name;
+  } catch (err) {
+    alert('LOCATION NOT FOUND')
+    console.log(err);
+  }
 }
